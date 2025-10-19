061814bab10d479e02dfea2a15e35eba43d1e390
Pushkar shirore <pushkarshirore2020@gmail.com>
2025-10-20 00:22:00 +0530

    P1

---
+++ 02_Machine_Learning/seession 11/P1/app.py
@@ -0,0 +1,24 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+
+    return render_template('index.html', prediction_text='Species = {}'.format(prediction[0]))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
